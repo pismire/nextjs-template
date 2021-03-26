c77abe137c877e4c292c628128654699195678ca
Helder Silva <helder.dds@gmail.com>
2021-03-25 21:32:55 -0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Template</title>
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to Next.js Template
+        </h1>
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
